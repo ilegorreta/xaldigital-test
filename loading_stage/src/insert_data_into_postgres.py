5a6eaca1f081653d2b853b1e4cfd09b2429f38cc
Ivan Legorreta <ilegorreta@hotmail.com>
2022-03-10 13:22:54 -0600

    Added validation rules for the employee CSV file

--- loading_stage/src/insert_data_into_postgres.py
+++ loading_stage/src/insert_data_into_postgres.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 from datetime import datetime
 
 
@@ -42,7 +45,28 @@ def insert_into_postgres():
     except Exception as e:
         print(f"ERROR: {e}")
 
+
+def state_validation(state):
+    if isinstance(state, str) and len(state) == 2:
+        return state
+    else:
+        raise ValueError(f"State value invalid: {state}")
+
+
+def validate_phone_number(df):
+    PHONE_REGEX = "\w{3}-\w{3}-\w{4}"
+    if re.search(PHONE_REGEX, df.phone1) and re.search(PHONE_REGEX, df.phone2):
+        pass
+    else:
+        raise ValueError(
+            f"Invalid phone number on employee: {df.first_name} {df.last_name}"
+        )
+
+
 def validate_fields():
+    df = pd.read_csv(f"data/employees.csv")
+    df["state"] = df["state"].apply(lambda x: state_validation(x))
+    df.apply(lambda x: validate_phone_number(x), axis=1)
 
 
 def main():
